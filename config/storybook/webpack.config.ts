9790486a5d901ca729d2e105327acdecdc786baa
Mary <mari.front.74@gmail.com>
2024-02-23 22:58:16 +0500

    fix storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,16 +1,15 @@
 import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
-import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import { BuildPaths } from '../build/types/config';
 
-export default ({ config }:{config:webpack.Configuration}) => {
-    const paths:BuildPaths = {
+export default ({ config }: {config: webpack.Configuration}) => {
+    const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
 
@@ -22,10 +21,12 @@ export default ({ config }:{config:webpack.Configuration}) => {
 
         return rule;
     });
+
     config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
     config.module.rules.push(buildCssLoader(true));
+
     return config;
 };
