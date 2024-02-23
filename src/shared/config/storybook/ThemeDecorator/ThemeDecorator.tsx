06d4f0aa95a8a341a4a9a8ead3431ef4aaa56605
Mary <mari.front.74@gmail.com>
2024-02-23 18:10:26 +0500

    2.27 back to legacy

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++
@@ -1,9 +0,0 @@
-import 'app/styles/index.scss';
-import { Story } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
-
-export const ThemeDecorator = (theme:Theme) => (StoryComponent: Story) => (
-    <div className={`app ${theme}`}>
-        <StoryComponent />
-    </div>
-);
