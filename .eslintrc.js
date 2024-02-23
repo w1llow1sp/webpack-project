28705b1e4ae2b64cd8bf318f2237964a39fdace8
Mary <mari.front.74@gmail.com>
2024-02-23 22:40:36 +0500

    add  "react/display-name": "off"

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        "react/display-name": "off",
         'i18next/no-literal-string': ['warn', { markupOnly: true, ignoreAttribute:['data-testid','to'] }],
     },
     globals: {
